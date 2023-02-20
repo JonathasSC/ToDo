99a179fb714d5cd25fbce4b93ad3b4198f0349f7
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-02-20 14:52:49 -0300

    static files bug fix

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about',
+    'tasks',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
