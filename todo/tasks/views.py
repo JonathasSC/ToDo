c2fc51c2df965a562f862b9bd83077c3cd134af7
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-02-22 18:29:59 -0300

    task viewer create

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Task
 
 def helloWorld(request):
 	return HttpResponse('Hello World!')
 
 def taskList(request):
-	return render(request, 'tasks/list.html')
+	tasks = Task.objects.all() #Pegando todos os objetos Tasks do banco de dados
+	return render(request, 'tasks/list.html', {'tasks': tasks} )
 
 def yourName(request, name):
 	return render(request, 'tasks/yourname.html',{'name': name})
+
+def taskView(request, id):
+	task = get_object_or_404(Task, pk=id)
+	return render(request, 'tasks/task.html',{'task': task})
