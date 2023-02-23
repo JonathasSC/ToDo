1414271d8f4bee900fde69ba96a812f301da9151
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-02-23 18:24:39 -0300

    created pagination

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('task/<int:id>',views.taskView, name='task-view'),
 	path('edit/<int:id>', views.editTask, name='edit-task'),
 	path('newtask/',views.newTask, name='new-task'),
+	path('delete/<int:id>',views.deleteTask, name='delete-task'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages #pacotes de mensagens para usuario
 
 def helloWorld(request):
 	return HttpResponse('Hello World!')
 
 def taskList(request):
-	tasks = Task.objects.all().order_by('-created_at') #Pegando todos os objetos Tasks do banco de dados
+	tasks_list = Task.objects.all().order_by('-created_at') #Pegando todos os objetos Tasks do banco de dados
+	
+	paginator = Paginator(tasks_list,3)
+	page = request.GET.get('page')
+	tasks = paginator.get_page(page)
+
 	return render(request, 'tasks/list.html', {'tasks': tasks} )
 
 def yourName(request, name):
@@ -44,3 +51,11 @@ def editTask(request,id):
 			return render(request, 'tasks/edittask.html',{'form': form, 'task': task})
 	else:
 		return render(request, 'tasks/edittask.html',{'form': form, 'task': task})
+
+def deleteTask(request, id):
+	task = get_object_or_404(Task, pk=id)
+	task.delete()
+
+	messages.info(request, 'Tarefa deletada com sucesso')
+
+	return redirect('/')
